f65a7878d1aede98a662d838f4779656b417485e
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-09-13 09:36:14 +0300

    feat: seo
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/landing-page/src/app/layout.tsx
+++ apps/landing-page/src/app/layout.tsx
@@ -4,8 +4,41 @@ import { Providers } from './providers';
 import localFont from 'next/font/local';
 
 export const metadata = {
-  title: 'SPT',
-  description: 'SPT',
+  title: 'SPT - لقطع غيار السيارات',
+  description: 'اطلب قطع غيار سيارتك بكل سهولة مع SPT!',
+  openGraph: {
+    type: 'website',
+    url: 'https://spt.sa/',
+    title: 'SPT - لقطع غيار السيارات',
+    description: 'اطلب قطع غيار سيارتك بكل سهولة مع SPT!',
+    images: [
+      {
+        url: 'https://spt.sa/car.svg',
+        alt: 'SPT - لقطع غيار السيارات',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'SPT - لقطع غيار السيارات',
+    description: 'اطلب قطع غيار سيارتك بكل سهولة مع SPT!',
+    images: [
+      {
+        url: 'https://spt.sa/car.svg',
+        alt: 'SPT - لقطع غيار السيارات',
+      },
+    ],
+  },
+  keywords: [
+    'قطع غيار',
+    'سيارات',
+    'SPT',
+    'قطع غيار السيارات',
+    'قطع غيار سيارات',
+    'قطع غيار السيارات الأصلية',
+    'قطع غيار السيارات المستعملة',
+    'تشاليح',
+  ],
 };
 
 const shubakFont = localFont({
