5e300fddc48f591926a3f1d270abd09c52ff8e76
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-10-31 07:49:35 +0300

    feat: mobile nav menu
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/app/src/features/navbar/navbar.tsx
+++ apps/app/src/features/navbar/navbar.tsx
@@ -20,48 +20,87 @@ import {
   NavbarMenuToggle,
 } from '@nextui-org/navbar';
 import { useTranslations } from 'next-intl';
-import { Link } from '@nextui-org/link';
 import { useUserStore } from '../auth/user-store';
 import { useLogout } from '../auth/login/use-logout';
 
 import {
   FaClipboardList,
+  FaFacebook,
   FaHome,
+  FaInstagram,
   FaServicestack,
+  FaTwitter,
   FaUserCircle,
   FaWallet,
+  FaWhatsapp,
 } from 'react-icons/fa';
 import { RiArrowDownSLine } from 'react-icons/ri';
 import NewNotificationSvg from './assets/new-notifications.svg';
+import MenuIcon from './assets/menu-icon.svg';
+import MenuIconOpened from './assets/menu-icon-opened.svg';
+import { Divider } from '@nextui-org/divider';
+import { cn } from '@nextui-org/theme';
+import Link from 'next/link';
+import { PrimaryButton } from '../../ui/primary-button';
+
+const menuItems = [
+  { label: 'home', href: '/home', icon: <FaHome /> },
+  {
+    label: 'services',
+    href: '/services',
+    icon: <FaServicestack />,
+  },
+  {
+    label: 'orders',
+    href: '/orders',
+    icon: <FaClipboardList />,
+  },
+  {
+    label: 'wallet',
+    href: '/wallet',
+    icon: <FaWallet />,
+  },
+  {
+    label: 'profile',
+    href: '/profile',
+    icon: <FaUserCircle />,
+  },
+];
+
+const socialLinks = [
+  {
+    label: 'Facebook',
+    href: 'https://www.facebook.com',
+    icon: <FaFacebook />,
+  },
+  {
+    label: 'Twitter',
+    href: 'https://twitter.com',
+    icon: <FaTwitter />,
+  },
+  {
+    label: 'Instagram',
+    href: 'https://www.instagram.com',
+    icon: <FaInstagram />,
+  },
+  {
+    label: 'WhatsApp',
+    href: 'https://www.whatsapp.com',
+    icon: <FaWhatsapp />,
+  },
+];
+
+const links = [
+  { label: 'about_app', href: '/about' },
+  { label: 'terms', href: '/terms' },
+  { label: 'privacy', href: '/privacy' },
+];
 
 export default function Navbar() {
-  const t = useTranslations();
+  const t = useTranslations('sidenav');
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const menuItems = [
-    { label: t('sidenav.home'), href: '/', icon: <FaHome /> },
-    {
-      label: t('sidenav.services'),
-      href: '/services',
-      icon: <FaServicestack />,
-    },
-    {
-      label: t('sidenav.orders'),
-      href: '/orders',
-      icon: <FaClipboardList />,
-    },
-    {
-      label: t('sidenav.wallet'),
-      href: '/wallet',
-      icon: <FaWallet />,
-    },
-    {
-      label: t('sidenav.profile'),
-      href: '/profile',
-      icon: <FaUserCircle />,
-    },
-  ];
-
+  const isLinkActive = (href: string) => location.pathname === href;
   const { user } = useUserStore();
   const { logout } = useLogout();
 
@@ -79,7 +118,7 @@ export default function Navbar() {
               inputWrapper:
                 'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/10',
             }}
-            placeholder={t('placeholder.search')}
+            placeholder={t('search')}
             size="sm"
             startContent={
               <SearchIcon className="fill-[#05B5B4] scale-x-[-1]" />
@@ -125,26 +164,92 @@ export default function Navbar() {
 
         {/* Mobile Menu Toggle */}
         <NavbarMenuToggle
-          className="ml-4 md:hidden"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          icon={
+            isMenuOpen ? (
+              <Image src={MenuIconOpened} alt={'Close menu'} />
+            ) : (
+              <Image src={MenuIcon} alt={'Open menu'} />
+            )
+          }
+          className="md:hidden h-9 w-9"
           aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
         />
       </NavbarContent>
 
       {/* Mobile Menu */}
-      <NavbarMenu>
-        {menuItems.map((item) => (
-          <NavbarMenuItem key={item.href}>
-            <Link
-              href={item.href}
-              color="foreground"
-              className="w-full flex items-center gap-1.5"
-            >
-              {item.icon}
-              <span className="ml-2">{item.label}</span>
-            </Link>
-          </NavbarMenuItem>
-        ))}
+      <NavbarMenu className="px-4 bg-white border border-t border-gray-100 md:hidden flex flex-col justify-between">
+        <div>
+          {menuItems.map((item) => (
+            <>
+              <NavbarMenuItem className={`py-4`} key={item.href}>
+                <Link
+                  href={item.href}
+                  color="foreground"
+                  // className={`w-full flex items-center gap-1.5 text-red-500 ${
+                  //   isLinkActive(item.href) && 'text-red-100'
+                  // }}`}
+                  className={cn(
+                    'w-full flex items-center gap-1.5 font-medium opacity-80',
+                    isLinkActive(item.href) && 'text-primary opacity-100'
+                  )}
+                >
+                  {item.icon}
+                  <span className="ml-2">{t(item.label)}</span>
+                </Link>
+              </NavbarMenuItem>
+              <Divider className={'bg-gray-100'} />
+            </>
+          ))}
+        </div>
+
+        <NavbarMenuItem className={`py-3`} key={'logout'}>
+          <PrimaryButton size={'lg'} text={'تسجيل الخروج'} onClick={logout} />
+
+          <div className="flex flex-col justify-center items-center gap-6 w-full mt-8">
+            <div className="flex justify-center items-center gap-4">
+              <span className="text-sm text-gray-600">{t('follow_us')}</span>
+              {socialLinks.map((link) => (
+                <Link
+                  href={link.href}
+                  key={link.label}
+                  target={'_blank'}
+                  className={'text-primary'}
+                >
+                  {link.icon}
+                </Link>
+              ))}
+            </div>
+
+            <div className="flex justify-center items-center gap-4">
+              {links.map((link) => (
+                <Link
+                  href={link.href}
+                  key={link.label}
+                  className={'text-xs font-semibold'}
+                >
+                  {t(link.label)}
+                </Link>
+              ))}
+            </div>
+
+            {/*  rights reserved */}
+            <RightsReserved />
+          </div>
+        </NavbarMenuItem>
       </NavbarMenu>
     </NextUINavbar>
   );
 }
+
+const RightsReserved = () => {
+  const t = useTranslations('sidenav');
+  const year = new Date().getFullYear();
+  return (
+    <div>
+      <span className="text-sm text-gray-500">
+        {t('rights_reserved') + ' ' + year + ' SPT©'}
+      </span>
+    </div>
+  );
+};
