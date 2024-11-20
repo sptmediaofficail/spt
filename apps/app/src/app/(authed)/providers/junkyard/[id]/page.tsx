55aa0d7491d0266fecf877ebceb5bd35390effc9
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-11-20 18:55:47 +0200

    stash: build
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/app/src/app/(authed)/providers/junkyard/[id]/page.tsx
+++ apps/app/src/app/(authed)/providers/junkyard/[id]/page.tsx
@@ -1,6 +1,21 @@
 import { ShowProviderPage } from '../../../../../features/providers/show-provider-page';
+import { fetchClient } from '../../../../../fetch-client';
 
-export default function JunkyardProviderPage({
+export const revalidate = 60;
+
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+export const dynamicParams = true; // or false, to 404 on unknown paths
+
+export async function generateStaticParams(): Promise<{ id: string }[]> {
+  const { data } = (await fetchClient.GET('/provider/ids')) as {
+    data: { data: Array<string> };
+  };
+  return data.data.map((id) => ({ id }));
+}
+
+export default async function JunkyardProviderPage({
   params,
 }: {
   params: { id: string };
--- apps/app/src/features/providers/show-provider-page.tsx
+++ apps/app/src/features/providers/show-provider-page.tsx
@@ -2,20 +2,6 @@
 // request comes in, at most once every 60 seconds.
 import {fetchClient} from '../../fetch-client';
 
-export const revalidate = 60;
-
-// We'll prerender only the params from `generateStaticParams` at build time.
-// If a request comes in for a path that hasn't been generated,
-// Next.js will server-render the page on-demand.
-export const dynamicParams = true; // or false, to 404 on unknown paths
-
-export async function generateStaticParams() {
-  const { data } = (await fetchClient.GET('/provider/ids')) as {
-    data: { id: string }[];
-  };
-  return data;
-}
-
 export const ShowProviderPage = async ({ id }: { id: string }) => {
   const { data: provider } = await fetchClient.GET('/provider/{id}', {
     params: {
