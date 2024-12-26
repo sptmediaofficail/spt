312759a451eb3b4fb7daa8e8b92949ec4985ebd8
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-12-26 02:09:42 +0200

    fix: map init position
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,2 @@
-const {nextui} = require('@nextui-org/theme');
 // just to get tailwindcss IntelliSense
 module.exports = {};
-  plugins: [nextui()],
-  content: [
-    "./node_modules/@nextui-org/theme/dist/components/(autocomplete|avatar|breadcrumbs|button|card|checkbox|date-picker|divider|dropdown|input|link|modal|navbar|pagination|select|skeleton|spacer|tabs|ripple|spinner|form|listbox|popover|scroll-shadow|calendar|date-input|menu).js"
-],
